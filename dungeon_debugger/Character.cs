2b3ceebc4db346f0a56d6ba22a2005cf7c918b31
bar-bund <a.riisovergaard@gmail.com>
2025-01-21 12:52:20 +0100

    cleanup Character.cs

--- dungeon_debugger/Character.cs
+++ dungeon_debugger/Character.cs
@@ -1,100 +1,92 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace dungeon_debugger;
 
-namespace dungeon_debugger
+// Base class for all characters
+public class Character
 {
-    // Base class for all characters
-    public class Character
+    public Character(string name, int health)
     {
-        public string Name { get; set; }
-        public int Health { get; set; }
-
-        public Character(string name, int health)
-        {
-            Name = name;
-            Health = health;
-        }
+        Name = name;
+        Health = health;
     }
 
-    // Player class
-    public class Player : Character
+    public string Name { get; set; }
+    public int Health { get; set; }
+}
+
+// Player class
+public class Player : Character
+{
+    public Player(string name) : base(name, 100)
     {
-        private List<Item> Inventory { get; set; }
-
-        public Player(string name) : base(name, 100)
-        {
-            Inventory = new List<Item>();
-        }
-
-        public int Attack(Item item)
-        {
-            Random random = new Random();
-            int baseDamage = random.Next(5, 15); // Random damage between 5 and 15
-
-            if (item is Vial)
-            {
-                baseDamage += ((Vial)item).DamageBoost;
-                Console.WriteLine("The Vial boosts your attack damage!");
-            }
-
-            return baseDamage;
-        }
-
-        public void Rest()
-        {
-            int healAmount = 10;
-            Health += healAmount;
-            Console.WriteLine($"You rest at the bonfire and recover {healAmount} health. Current health: {Health}.");
-        }
-
-        public void AddToInventory(Item item)
-        {
-            Inventory.Add(item);
-            Console.WriteLine($"{item.Name} has been added to your inventory.");
-        }
-
-        public void ViewInventory()
-        {
-            Console.WriteLine("Your Inventory:");
-            foreach (var item in Inventory)
-            {
-                Console.WriteLine($"- {item.Name}");
-            }
-        }
+        Inventory = new List<Item>();
     }
 
-    // Enemy class
-    public class Enemy : Character
+    private List<Item> Inventory { get; }
+
+    public int Attack(Item item)
     {
-        private static Random random = new Random();
+        var random = new Random();
+        var baseDamage = random.Next(5, 15); // Random damage between 5 and 15
 
-        public Enemy(string name, int health) : base(name, health) { }
-
-        public int Attack()
+        if (item is Vial)
         {
-            return random.Next(5, 10); // Random damage between 5 and 10
+            baseDamage += ((Vial)item).DamageBoost;
+            Console.WriteLine("The Vial boosts your attack damage!");
         }
 
-        public Item DropItem()
+        return baseDamage;
+    }
+
+    public void Rest()
+    {
+        var healAmount = 10;
+        Health += healAmount;
+        Console.WriteLine($"You rest at the bonfire and recover {healAmount} health. Current health: {Health}.");
+    }
+
+    public void AddToInventory(Item item)
+    {
+        Inventory.Add(item);
+        Console.WriteLine($"{item.Name} has been added to your inventory.");
+    }
+
+    public void ViewInventory()
+    {
+        Console.WriteLine("Your Inventory:");
+        foreach (var item in Inventory) Console.WriteLine($"- {item.Name}");
+    }
+}
+
+// Enemy class
+public class Enemy : Character
+{
+    private static readonly Random random = new();
+
+    public Enemy(string name, int health) : base(name, health)
+    {
+    }
+
+    public int Attack()
+    {
+        return random.Next(5, 10); // Random damage between 5 and 10
+    }
+
+    public Item DropItem()
+    {
+        List<Item> possibleItems = new()
         {
-            List<Item> possibleItems = new List<Item>
-            {
-                new Vial(),
-                new Bandage(),
-                new Shield()
-            };
+            new Vial(),
+            new Bandage(),
+            new Shield()
+        };
 
-            if (random.Next(2) == 0) // 50% chance of dropping an item
-            {
-                Item droppedItem = possibleItems[random.Next(possibleItems.Count)];
-                Console.WriteLine($"The {Name} dropped a {droppedItem.Name}!");
-                return droppedItem;
-            }
-
-            return null;
+        if (random.Next(2) == 0) // 50% chance of dropping an item
+        {
+            var droppedItem = possibleItems[random.Next(possibleItems.Count)];
+            Console.WriteLine($"The {Name} dropped a {droppedItem.Name}!");
+            return droppedItem;
         }
+
+        return null;
     }
 }
